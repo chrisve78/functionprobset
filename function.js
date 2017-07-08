42399d92f6ba5d82f4b2230b209d4f5ffe02e8b7
Chris <cellis8200@gmail.com>
2017-07-08 11:42:44 -0700

    first commit

---
+++ function.js
@@ -0,0 +1,32 @@
+//Write a function isEven() which takes a single numeric argument and returns true if
+//the number is even and false otherwise.
+function isEven(number){
+	if(number % 2 === 0){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+//Write a function factorial() which takes a single numeric argument and returns
+//the factorial of that number
+// 4! is 4 x 3 x 2 x 1
+// 6! is 6 x 5 x 4 x 3 x 2 x 1
+// 0! is 1
+function factorial(number){
+	if(number < 0){
+		return;
+	}else if(number === 0){
+		return 1;
+	}else{
+		return number * factorial(number-1);
+	}
+
+}
+
+//Write a function kebabToSnake() which takes a single kebab cased string argument
+// and returns the snake_cased_version. Replace "-" with "_"
+function kebabToSnake(string){
+	var result = string.replace("-", "_")
+	return result;
+}
