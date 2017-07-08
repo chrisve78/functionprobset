b01bf11d60e10c8bf22ecd95e751c1d9dce25d53
Chris <cellis8200@gmail.com>
2017-07-08 11:59:53 -0700

    refactor

--- function.js
+++ function.js
@@ -1,11 +1,12 @@
 //Write a function isEven() which takes a single numeric argument and returns true if
 //the number is even and false otherwise.
 function isEven(number){
-	if(number % 2 === 0){
-		return true;
-	}else{
-		return false;
-	}
+	// if(number % 2 === 0){
+	// 	return true;
+	// }else{
+	// 	return false;
+	// }
+	return number % 2 === 0;
 }
 
 //Write a function factorial() which takes a single numeric argument and returns
